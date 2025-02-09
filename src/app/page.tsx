584f1f376137082e0488638db48a583949d7b051
Tal Hayut <talhayut.dev@gmail.com>
2025-02-09 09:57:42 +0200

    chore: branding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,47 @@
-const HomePage = () => {
-  return <h1>hello</h1>
+'use client'
+
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+import React from 'react'
+
+import styles from './page.module.scss'
+
+const Navbar: React.FC = () => {
+  const path = usePathname()
+
+  const navItems = [
+    { name: 'Home', path: '/' },
+    { name: 'Shaders', path: '/shaders' }
+  ]
+
+  return (
+    <nav className={styles.navbar}>
+      <div className={styles.navContainer}>
+        {navItems.map((item) => (
+          <Link
+            key={item.name}
+            href={item.path}
+            className={`${styles.navItem} ${
+              path === item.path ? styles.active : ''
+            }`}
+          >
+            {item.name}
+          </Link>
+        ))}
+      </div>
+    </nav>
+  )
 }
 
-export default HomePage
+const Home: React.FC = () => {
+  return (
+    <div className={styles.pageContainer}>
+      <Navbar />
+      <main className={styles.main}>
+        <h1 className={styles.heading}>Welcome to T.H.E LAB</h1>
+      </main>
+    </div>
+  )
+}
+
+export default Home
